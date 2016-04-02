e2bfc809e45a5b8a4b73508be120008f7f80bcce
Teffen Ellis (Eric) <teffen@nirri.us>
2016-04-02 16:44:22 -0700

    Flesh out boilerplate.

---
+++ build/app.js
@@ -0,0 +1,28 @@
+(function () {
+  // Check for IE9+
+  if (!window.addEventListener) return;
+
+  const UPDATE_DELAY = 1500;
+
+  let options = INSTALL_OPTIONS;
+
+  function unmountNode(node) {
+    if (node && node.parentNode) node.parentNode.removeChild(node);
+  }
+
+  function updateElement() {}
+
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", updateElement);
+  } else {
+    updateElement();
+  }
+
+  INSTALL_SCOPE = {
+    setOptions(nextOptions) {
+      options = nextOptions;
+
+      updateElement();
+    }
+  };
+})();
