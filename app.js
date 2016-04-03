496369fbc2d48af2c3edd003addc9f2ee53ca3ec
Teffen Ellis (Eric) <teffen@nirri.us>
2016-04-02 19:17:06 -0700

    Flesh out configurator, rendering.

--- app.js
+++ app.js
@@ -1,30 +1,50 @@
 (function() {
-  // Check for IE9+
-  if (!window.addEventListener) return
+  "use strict"
 
-  const UPDATE_DELAY = 1500
+  if (!window.addEventListener) return // Check for IE9+
 
-  let options= INSTALL_OPTIONS
+  let element
+  let options = INSTALL_OPTIONS
 
-  function unmountNode(node) {
-    if (node && node.parentNode) node.parentNode.removeChild(node)
+  function render({content, imageSrc, path}) {
+    element = Eager.createElement(options.location, element)
+
+    element.innerHTML = content.html
+    element.classList.add("eager-article")
+    element.dataset.eagerArticlePath = path
+
+    if (imageSrc) {
+      const {firstChild} = element
+      const image = Object.assign(document.createElement("img"), {
+        className: "eager-hero",
+        src: imageSrc
+      })
+
+      firstChild ? element.insertBefore(image, firstChild) : element.appendChild(image)
+    }
   }
 
-  function updateElement() {
+  function checkHash() {
+    const path = window.location.hash.split("#!")[1] || "/"
+    const article = options.articles.find($ => $.path === path)
+
+    if (article) render(article)
   }
 
   if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", updateElement)
+    document.addEventListener("DOMContentLoaded", checkHash)
   }
   else {
-    updateElement()
+    checkHash()
   }
+
+  window.addEventListener("hashchange", checkHash)
 
   INSTALL_SCOPE = {
     setOptions(nextOptions) {
       options = nextOptions
 
-      updateElement()
+      checkHash()
     }
   }
 }())
--- build/app.js
+++ build/app.js
@@ -1,28 +1,59 @@
+"use strict";
+
 (function () {
-  // Check for IE9+
-  if (!window.addEventListener) return;
+  "use strict";
 
-  const UPDATE_DELAY = 1500;
+  if (!window.addEventListener) return; // Check for IE9+
 
-  let options = INSTALL_OPTIONS;
+  var element = void 0;
+  var options = INSTALL_OPTIONS;
 
-  function unmountNode(node) {
-    if (node && node.parentNode) node.parentNode.removeChild(node);
+  function render(_ref) {
+    var content = _ref.content;
+    var imageSrc = _ref.imageSrc;
+    var path = _ref.path;
+
+    element = Eager.createElement(options.location, element);
+
+    element.innerHTML = content.html;
+    element.classList.add("eager-article");
+    element.dataset.eagerArticlePath = path;
+
+    if (imageSrc) {
+      var _element = element;
+      var firstChild = _element.firstChild;
+
+      var image = Object.assign(document.createElement("img"), {
+        className: "eager-hero",
+        src: imageSrc
+      });
+
+      firstChild ? element.insertBefore(image, firstChild) : element.appendChild(image);
+    }
   }
 
-  function updateElement() {}
+  function checkHash() {
+    var path = window.location.hash.split("#!")[1] || "/";
+    var article = options.articles.find(function ($) {
+      return $.path === path;
+    });
+
+    if (article) render(article);
+  }
 
   if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", updateElement);
+    document.addEventListener("DOMContentLoaded", checkHash);
   } else {
-    updateElement();
+    checkHash();
   }
 
+  window.addEventListener("hashchange", checkHash);
+
   INSTALL_SCOPE = {
-    setOptions(nextOptions) {
+    setOptions: function setOptions(nextOptions) {
       options = nextOptions;
 
-      updateElement();
+      checkHash();
     }
   };
 })();
